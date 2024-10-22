7e313d3677e627cd2287c53eaf0af4a07acbc74c
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-10-22 08:19:57 +0300

    Add safe interfaces for the cJSON_Hooks struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,112 @@ use bindings::*;
 pub const VERSION_MAJOR: u32 = bindings::CJSON_VERSION_MAJOR;
 pub const VERSION_MINOR: u32 = bindings::CJSON_VERSION_MINOR;
 pub const VERSION_PATCH: u32 = bindings::CJSON_VERSION_PATCH;
-pub const INVALID: u32 = bindings::cJSON_Invalid;
-pub const FALSE: u32 = bindings::cJSON_False;
-pub const TRUE: u32 = bindings::cJSON_True;
-pub const NULL: u32 = bindings::cJSON_NULL;
-pub const NUMBER: u32 = bindings::cJSON_Number;
-pub const STRING: u32 = bindings::cJSON_String;
-pub const ARRAY: u32 = bindings::cJSON_Array;
-pub const OBJECT: u32 = bindings::cJSON_Object;
-pub const RAW: u32 = bindings::cJSON_Raw;
-pub const IS_REFERENCE: u32 = bindings::cJSON_IsReference;
-pub const STRING_IS_CONST: u32 = bindings::cJSON_StringIsConst;
-pub const NESTING_LIMIT: u32 = bindings::CJSON_NESTING_LIMIT;
-pub const CIRCULAR_LIMIT: u32 = bindings::CJSON_CIRCULAR_LIMIT;
+pub const IS_REFERENCE: u32 = 256;
+pub const STRING_IS_CONST: u32 = 512;
+pub const NESTING_LIMIT: u32 = 1000;
+pub const CIRCULAR_LIMIT: u32 = 10000;
+
+pub enum JsonType {
+    Invalid = 0,
+    False = 1,
+    True = 2,
+    Null = 4,
+    Number = 8,
+    String = 16,
+    Array = 32,
+    Object = 64,
+    Raw = 128,
+}
 
 pub fn cjson_version() -> String {
     format!("{}.{}.{}", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
+}
+
+pub struct Hooks {
+    pub malloc_fn: Option<fn(sz: usize) -> *mut libc::c_void>,
+    pub free_fn: Option<fn(*mut libc::c_void)>,
+}
+
+impl Hooks {
+    /// Create new instance of the Hooks struct with optional custom `malloc`
+    /// and `free` functions.
+    ///
+    /// If no functions are provided (passing `None` to the constructor), the
+    /// cJSON library will use the default `malloc` and `free` functions from C.
+    ///  
+    /// Example:
+    /// ```rust
+    /// use cjson_rs::Hooks;
+    /// use libc::{malloc, free};
+    ///
+    /// fn custom_malloc(size: usize) -> *mut libc::c_void {
+    ///     println!("allocating memory ...");
+    ///     unsafe { malloc(size) }
+    /// }
+    ///
+    /// fn custom_free(ptr: *mut libc::c_void) {
+    ///    println!("freeing memory ...");
+    ///    unsafe { free(ptr); }
+    /// }
+    ///
+    /// fn main() {
+    ///     let _: Hooks = Hooks::new(Some(custom_malloc), Some(custom_free));
+    ///     let _: Hooks = Hooks::new(None, None); // the default C functions will be used
+    /// }
+    ///
+    /// ```
+    pub fn new(
+        malloc_fn: Option<fn(usize) -> *mut libc::c_void>,
+        free_fn: Option<fn(*mut libc::c_void)>,
+    ) -> Hooks {
+        Hooks { malloc_fn, free_fn }
+    }
+
+    // map Hooks instance to cJSON_Hooks instance
+    fn to_cjson_hooks(&self) -> cJSON_Hooks {
+        cJSON_Hooks {
+            malloc_fn: self.malloc_fn.map(|f| {
+                static mut RUST_MALLOC_FN: Option<fn(usize) -> *mut libc::c_void> = None;
+                unsafe { RUST_MALLOC_FN = Some(f) };
+
+                unsafe extern "C" fn c_malloc(sz: usize) -> *mut libc::c_void {
+                    if let Some(f) = RUST_MALLOC_FN {
+                        let boxed = Box::new(f(sz));
+                        Box::into_raw(boxed) as *mut libc::c_void
+                    } else {
+                        std::ptr::null_mut()
+                    }
+                }
+
+                c_malloc as unsafe extern "C" fn(usize) -> *mut libc::c_void
+            }),
+            free_fn: self.free_fn.map(|f| {
+                static mut RUST_FREE_FN: Option<fn(*mut libc::c_void)> = None;
+                unsafe { RUST_FREE_FN = Some(f) };
+
+                unsafe extern "C" fn c_free(ptr: *mut libc::c_void) {
+                    if let Some(f) = RUST_FREE_FN {
+                        if !ptr.is_null() {
+                            f(ptr)
+                        }
+                    }
+                }
+
+                c_free as unsafe extern "C" fn(*mut libc::c_void)
+            }),
+        }
+    }
+
+    /// Initialize the custom memory management hooks
+    ///
+    /// Usage:
+    /// ```rust
+    /// let hooks = Hooks::new(Some(custom_malloc), Some(custom_free));
+    /// hooks.init();
+    /// ```
+    pub fn init(&self) {
+        unsafe {
+            cJSON_InitHooks(&mut self.to_cjson_hooks());
+        }
+    }
 }
