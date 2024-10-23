ff64ce3b85cbcaf4f6b5b09243c692273c325e65
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-10-23 12:31:37 +0300

    Add function to create Json item of type Array containing double-precision floating-point values

--- src/lib.rs
+++ src/lib.rs
@@ -1041,3 +1041,30 @@ pub fn cjson_create_int_array(numbers: *const i32, count: i32) -> *mut Json {
 pub fn cjson_create_float_array(numbers: *const f32, count: i32) -> *mut Json {
     unsafe { cJSON_CreateFloatArray(numbers, count) as *mut Json }
 }
+
+/// Create Json item of type `Array` containing double-precision floating-point values.
+///
+/// Args:
+/// - `numbers: *const i64` - Pointer to a signed double-precision floating-point value (start of the
+/// numbers array).
+/// - `count: i32` - Number of array elements to include in the `Array` being created (typically just the
+/// size of the `numbers` array).
+///
+/// Returns:
+/// - `*mut Json` - a mutable pointer to the created Json item of type `Array` containing double-precision
+/// floating-point values.
+///
+/// Example:
+/// ```rust
+/// use cjson_rs::*;
+///
+/// fn main() {
+///     let numbers: [f64; 5] = [1.0, 2.0, 3.0, 4.0, 5.0];
+///     let json = cjson_create_double_array(&numbers[0], numbers.len() as i32);
+///     assert_eq!(json.is_type_array(), true);
+///     println!("Test passed"); // output: Test passed
+/// }
+/// ```
+pub fn cjson_create_double_array(numbers: *const f64, count: i32) -> *mut Json {
+    unsafe { cJSON_CreateDoubleArray(numbers, count) as *mut Json }
+}
