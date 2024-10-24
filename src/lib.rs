30e34e3992487e2cb1fa1d603d58e39221c43bd6
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-10-24 17:42:11 +0300

    Add function to get item within a Json item of type `Object` with the specified key, with a case-sensitive comparison of keys

--- src/lib.rs
+++ src/lib.rs
@@ -2125,3 +2125,46 @@ pub fn cjson_get_object_item(object: *mut Json, string: &str) -> Result<*mut Jso
         Err(err) => Err(JsonError::CStringError(err)),
     }
 }
+
+/// Get item within the object with the specified key, with a case-sensitive comparison of keys.
+///
+/// Args:
+/// - `object: *mut Json` - Json item of type `Object` from which we want to get an item.
+/// - `string: &str` - Key of the Json item that we want to get.
+///
+/// Returns:
+/// - `Ok(*mut Json)` - a mutable pointer to the Json item with the provided key if gotten successfully.
+/// - `Err(JsonError::CStringError(NulError))` - if the provided string slice contains a null byte.
+///
+/// Example:
+/// ```rust
+/// use cjson_rs::*;
+///
+/// fn main() {
+///     let object = cjson_create_object();
+///     cjson_add_string_to_object(object, "name", "Nemuel").unwrap();
+///
+///     let item = cjson_get_object_item_case_sensitive(object, "Name").unwrap();
+///     assert_eq!(item.is_null(), true);
+///     let item = cjson_get_object_item_case_sensitive(object, "name").unwrap();
+///     assert_eq!(item.is_null(), false);
+///     assert_eq!(item.is_type_string(), true);
+///
+///     println!("Test passed"); // output: Test passed
+/// }
+/// ```
+pub fn cjson_get_object_item_case_sensitive(
+    object: *mut Json,
+    string: &str,
+) -> Result<*mut Json, JsonError> {
+    match CString::new(string) {
+        Ok(c_str) => {
+            let result = unsafe {
+                cJSON_GetObjectItemCaseSensitive(object as *const cJSON, c_str.as_ptr())
+                    as *mut Json
+            };
+            Ok(result)
+        }
+        Err(err) => Err(JsonError::CStringError(err)),
+    }
+}
